b10cb6e9315dacc9b6d31fb9b88238ece156a7ad
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-06 20:49:55 +0300

    add foreach исправленное

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -17,11 +17,12 @@ int GetSumNums(int arg)
 Console.Write($"Sum numbers: {GetSumNums(number)}");
 
 
+int[] array = {11,21,31,41,15,61,17,18,19};
 
 int positivesum = 0;
 int negativsum = 0;
 foreach(int el in array)
 {
-    if(el > 0) positivesum += el
-    else negativsum += el
+    if(el > 0) positivesum += el;
+    else negativsum += el;
 }
