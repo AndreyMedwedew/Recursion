f873d46c37232d63221f8aa16c25306ec2441bf9
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-06 19:30:01 +0300

    add function GetSumNums

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -2,3 +2,16 @@
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine("Число равно " + number);
+
+int GetSumNums(int arg)
+{
+    int sum = 0;
+    while(arg > 0)
+    {
+        sum += arg;
+        arg -= 1;
+    }
+    return sum;
+}
+
+Console.Write($"Sum numbers: {GetSumNums(number)}");
